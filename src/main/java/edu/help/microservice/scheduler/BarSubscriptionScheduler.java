f973c9ba647a77bdf8bca96fd504df6e3fba9476
bubbarob19 <bubbarob19@gmail.com>
2025-01-01 13:17:52 -0600

    scheduler

---
+++ src/main/java/edu/help/microservice/scheduler/BarSubscriptionScheduler.java
@@ -0,0 +1,13 @@
+package edu.help.microservice.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BarSubscriptionScheduler {
+
+    @Scheduled(cron = "0 0 0 1 * ?")
+    public void chargeMonthlySubscriptions() {
+        // Add logic here
+    }
+}
