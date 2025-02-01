e068ec988d5994823c8da5fda9f1f95753231a21
Lako35 <wesleyyu1223@gmail.com>
2025-01-31 19:13:47 -0500

    Integration of getTips untested

---
+++ src/main/java/edu/help/microservice/dto/GetTipsRequest.java
@@ -0,0 +1,13 @@
+package edu.help.microservice.dto;
+
+public class GetTipsRequest {
+    private String bartenderID;
+
+    public String getBartenderID() {
+        return bartenderID;
+    }
+
+    public void setBartenderID(String bartenderID) {
+        this.bartenderID = bartenderID;
+    }
+}
